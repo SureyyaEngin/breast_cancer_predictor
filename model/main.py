709fb4abcde40ed437f03113df2c8f544e9540b4
Süreyya Engin <sureyyaengin529@gmail.com>
2025-05-04 18:22:37 +0300

    scaling and splitting data

--- model/main.py
+++ model/main.py
@@ -1,4 +1,27 @@
 import pandas as pd
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+
+def create_model(data):
+    X = data.drop(['diagnosis'], axis=1)
+    y = data['diagnosis']
+
+    #scale the data
+    scaler = StandardScaler()
+    X = scaler.fit_transform(X)
+
+    #split the data
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+    #train
+    model = LogisticRegression()
+    model.fit(X_train, y_train)
+
+    return model, scaler
+
+#def test_model(model):
+
 
 def get_clean_data():
     data = pd.read_csv("data/data.csv")
@@ -10,7 +33,10 @@ def get_clean_data():
 
 def main():
     data = get_clean_data()
-    print(data.info())
+    model, scaler = create_model(data)
+
+    #test_model(model)
+
 
 if __name__ == '__main__':
     main()
