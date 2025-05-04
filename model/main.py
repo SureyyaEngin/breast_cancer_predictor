ecd20d5eff601924b76dc10097edda505338df1c
Süreyya Engin <sureyyaengin529@gmail.com>
2025-05-04 18:38:40 +0300

    test the model

--- model/main.py
+++ model/main.py
@@ -2,6 +2,8 @@ import pandas as pd
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, classification_report
+
 
 def create_model(data):
     X = data.drop(['diagnosis'], axis=1)
@@ -14,13 +16,17 @@ def create_model(data):
     #split the data
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-    #train
+    #train the model
     model = LogisticRegression()
     model.fit(X_train, y_train)
 
+    #test model
+    y_pred = model.predict(X_test)
+    print('Accuracy of our model: ', accuracy_score(y_test, y_pred))
+    print("Classification_report: \n", classification_report(y_test, y_pred))
+
     return model, scaler
 
-#def test_model(model):
 
 
 def get_clean_data():
@@ -34,9 +40,6 @@ def get_clean_data():
 def main():
     data = get_clean_data()
     model, scaler = create_model(data)
-
-    #test_model(model)
-
 
 if __name__ == '__main__':
     main()
